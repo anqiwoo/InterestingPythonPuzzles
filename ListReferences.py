8e46ae8a4585bf82231328de7f84848100a94bd1
anqiwu85 <64836742+anqiwoo@users.noreply.github.com>
2021-12-26 11:25:24 +0800

    Update ListReferences.py

--- ListReferences.py
+++ ListReferences.py
@@ -15,5 +15,10 @@ print(t is s2)
 This puzzle is really tricky because it goes into the details of Python.
 In general you can say that x += 1 is the short form of x = x + 1.
 However, if you work with non-primitive data types like lists, this is not always true.
-In the puzzle we have a variable t which points to a list. Then, we create two more variable which point to the same list in memory. The interesting thing comes in the third line. Writing s1 = s1 + [1] creates a copy of list s1 and appends [1] to this copied list. In the fourth line where we write s2 += [1] the append happens without creating a new list! Therefore the value is appended to the list to which t and s2 are still pointing to. So the final output is [0, 1] and s1 = [0, 1] also. However t is s2 is True whereas t is s1 is False! Bear in mind: When working with lists += is different from +!
+In the puzzle we have a variable t which points to a list. 
+Then, we create two more variable which point to the same list in memory. 
+The interesting thing comes in the third line. Writing s1 = s1 + [1] creates a copy of list s1 and appends [1] to this copied list. 
+In the fourth line where we write s2 += [1] the append happens without creating a new list! 
+Therefore the value is appended to the list to which t and s2 are still pointing to. So the final output is [0, 1] and s1 = [0, 1] also. 
+However t is s2 is True whereas t is s1 is False! Bear in mind: When working with lists += is different from +!
 '''
