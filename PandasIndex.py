42f2406f37b81d877b87fdca2a8343205c478a6b
anqiwoo <anqiwu85@outlook.com>
2022-01-05 21:58:38 +0800

    Update Past Puzzle

--- PandasIndex.py
+++ PandasIndex.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
-xs = pd.Series([5, 1, 3, 2, 4, ])
-print(xs, '\n')
+xs = pd.Series([5, 1, 3, 2, 4])
+print(xs)
+print(xs[0])
+print(xs.iloc[0])
+
+
+print('\n')
 
 xs.sort_values(inplace=True)
 print(xs)
-
+print(xs[0])
+print(xs.iloc[0])
 print(xs[0] == xs.iloc[0])  # Imagine the i in iloc is index.
 print(xs[0] == xs.loc[0])  # Imagine the l in loc is label.
-# If you omit the iloc or loc, you actually go with the loc method.
+# If you omit the iloc or loc, you actually go with the loc method or label method.
