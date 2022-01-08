cfaae792facbc28a6a267737414d2c133d8c5655
anqiwoo <anqiwu85@outlook.com>
2022-01-08 09:58:39 +0800

    2022/1/8 Python Puzzle

--- Numpy_Diff.py
+++ Numpy_Diff.py
@@ -12,3 +12,14 @@ diffs_axis0 = np.diff(a, n=2, axis=0)
 print(a, '\n')
 print(diffs_axis1, '\n')
 print(diffs_axis0)
+
+'''
+The numpy diff function calculates the difference between two subsequent values of a NumPy array.
+Hence, an array with n elements results in a diff array with (n-1) elements.
+
+When defining the parameter n, the diff function is applied n times to the output of the previous diff function execution.
+Thus, the first row undergoes the following transformations:
+
+[0 1 1] *diff*--> [1 0] *diff*--> [-1]
+
+'''
