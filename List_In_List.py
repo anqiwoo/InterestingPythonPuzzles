59263db7b8b2f5333e24336a1f42b3a55745a909
anqiwoo <anqiwu85@outlook.com>
2022-01-12 20:20:40 +0800

    22/1/12 Python Puzzle

---
+++ List_In_List.py
@@ -0,0 +1,12 @@
+python = ['hooray']
+x = python in python
+print(x)
+
+'''
+We create a list called "python". This list contains one element, the string 'hooray'.
+Using the keyword in, we check if the list python is contained In itself. 
+This can never be true. 
+
+Imagine you have an empty list. 
+If the empty list was contained in the empty list, the list wasn't empty!
+'''
