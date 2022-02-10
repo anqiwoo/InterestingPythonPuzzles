6603605fb8e229d252cc18d46022ebaecdf523d2
anqiwoo <anqiwu85@outlook.com>
2022-02-10 15:02:06 +0800

    22/2/10 Python Practice

--- fib_generator_function.py
+++ fib_generator_function.py
@@ -4,9 +4,22 @@ def fib_generator(max):
         yield b
         a, b = b, a + b
         n += 1
+    return 'done'
 
 
 g = fib_generator(10)
 print(type(g))
-for i in g:
-    print(i)
+
+while True:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+# for i in g:
+#     print(i)
+'''
+用for循环调用generator时，拿不到generator的return语句的返回值。如果想要拿到返回值，必须捕获StopIteration错误，返回值包含在StopIteration的value中。
+'''
