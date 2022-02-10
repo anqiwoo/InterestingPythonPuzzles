b9b8ece0b4024e3a20f3714720c01fb692c53f06
anqiwoo <anqiwu85@outlook.com>
2022-02-10 14:17:29 +0800

    22/2/10 Python Practice

--- iterable_judge.py
+++ iterable_judge.py
@@ -3,7 +3,8 @@ from collections.abc import Iterable
 s = 'abc'
 l = [1, 2, 3]
 i = 123
+g = (i for i in l)
 
-judge_list = [s, l, i]
+judge_list = [s, l, i, g]
 
 print([isinstance(item, Iterable) for item in judge_list])
