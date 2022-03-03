4ad9c211768f7a7b2be91ee2412d1e2238ae181d
anqiwoo <anqiwu85@outlook.com>
2022-03-03 09:45:52 +0800

    update on 22/3/2

--- Logical_Statement_Priority.py
+++ Logical_Statement_Priority.py
@@ -1,3 +1,3 @@
-# The Priority of Logical Statements in Python: Not > And > Or
+# The Priority of Logical Statements in Python: Not > And > Or 【“NAO”】
 print(not 1 and 0 or 1)  # 1
 print(not 1 or 1 and 0)  # 0
