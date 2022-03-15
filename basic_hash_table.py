d111780e58d0619c362a8ca149103a8146565c6a
anqiwoo <anqiwu85@outlook.com>
2022-03-15 15:01:18 +0800

    22/3/15 Python Fun

--- basic_hash_table.py
+++ basic_hash_table.py
@@ -5,7 +5,18 @@ import random
 
 
 def basic_hash_table(value_l, n_buckets=5):
+    '''
+    Input:
+        - value_l: a list of integer numbers
+        - n_buckets: desired amount of buckets. 
+
+    Output:
+        - hash_table: a hash table stored as a dictionary, where keys contain the hash keys, and the values will provide the hashed elements of the input list. 
+    '''
     def hash_function(value):
+        '''
+        The hash function is just the remainder of the integer division between each element and the desired number of buckets.
+        '''
         return int(value) % n_buckets
     hash_table = {i: [] for i in range(n_buckets)}
     for value in value_l:
