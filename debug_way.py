d25de71dbfa3725135707a7df7b5bb41b79d5cf4
anqiwoo <anqiwu85@outlook.com>
2022-03-19 10:04:32 +0800

    22/3/19 Python Fun

---
+++ debug_way.py
@@ -0,0 +1,2 @@
+# 1. print()： the simplest way to debug. You can add print() to somewhere you think could go wrong.
+# 2. assert: e.g. assert isinstance(n, int), 'n is not an integer!' 如果断言assert失败，assert语句本身就会抛出AssertionError
--- raise_error.py
+++ raise_error.py
@@ -6,10 +6,12 @@ We can use raise error to catch exceptions and end the execution right away when
 def take_an_integer(n):
     if not isinstance(n, int):
         raise ValueError('Invalid input! Please pass an integer.')
+    # functionally same as:
+    # assert isinstance(n, int), 'Invalid input! Please pass an integer.'
     print(n)
 
 
 if __name__ == '__main__':
-    # take_an_integer('a')
+    take_an_integer('a')
     take_an_integer(1)
     print('End!')
