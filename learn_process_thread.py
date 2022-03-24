e73be1d964d60a62efa07b7dd920d9b2bcfd15b0
anqiwoo <anqiwu85@outlook.com>
2022-03-24 11:39:25 +0800

    22/3/24 Python Fun

--- learn_process_thread.py
+++ learn_process_thread.py
@@ -1,9 +1,12 @@
 '''
 # Conclusion
-    1. Python既支持多进程，又支持多线程
+    1. Python既支持多进程multi-processing，又支持多线程multi-threading
     2. 线程是最小的执行单元，而进程由至少一个线程组成。
     3. 如何调度进程和线程，完全由操作系统决定，程序自己不能决定什么时候执行，执行多长时间。
     4. 多进程和多线程的程序涉及到同步、数据共享的问题，编写起来更复杂。
+    5. 在Unix/Linux下，可以使用os.fork()调用实现多进程。
+    6. 要实现跨平台的多进程，可以使用multiprocessing模块。
+    7. 进程间通信是通过Queue、Pipes等实现的。
 
 对于操作系统来说，一个任务就是一个进程（Process）。
 在一个进程Process内部，要同时干多件事，就需要同时运行多个“子任务”，我们把进程内的这些“子任务”称为线程（Thread）。
@@ -33,3 +36,122 @@
 '''
 
 # 多进程（multiprocessing）
+'''
+如果你打算编写多进程的服务程序，Unix/Linux无疑是正确的选择。
+由于Windows没有fork调用，难道在Windows上无法用Python编写多进程的程序？
+由于Python是跨平台的，自然也应该提供一个跨平台的多进程支持。
+
+# Fork
+有了fork调用，一个进程在接到新任务时就可以复制出一个子进程来处理新任务，
+常见的Apache服务器就是由父进程监听端口，每当有新的http请求时，就fork出子进程来处理新的http请求。
+
+# Process
+multiprocessing模块就是跨平台版本的多进程模块。
+multiprocessing模块提供了一个Process类来代表一个进程对象。
+创建子进程时，只需要传入一个执行函数和函数的参数，创建一个Process实例，
+用start()方法启动，这样创建进程比fork()还要简单。
+join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步。
+
+# Pool
+如果要启动大量的子进程，可以用进程池的方式批量创建子进程。
+Pool的默认大小是CPU的核数。
+
+# subprocess
+很多时候，子进程并不是自身，而是一个外部进程。我们创建了子进程后，还需要控制子进程的输入和输出。
+subprocess模块可以让我们非常方便地启动一个子进程，然后控制其输入和输出。
+
+# 进程间通信
+Process之间肯定是需要通信的，操作系统提供了很多机制来实现进程间的通信。
+Python的multiprocessing模块包装了底层的机制，提供了Queue、Pipes等多种方式来交换数据。
+
+# 关于Windows
+在Unix/Linux下，multiprocessing模块封装了fork()调用，使我们不需要关注fork()的细节。
+由于Windows没有fork调用，因此，multiprocessing需要“模拟”出fork的效果，
+父进程所有Python对象都必须通过pickle序列化再传到子进程去，
+所以，如果multiprocessing在Windows下调用失败了，要先考虑是不是pickle失败了。
+
+'''
+
+
+
+
+import os
+import time
+import random
+from multiprocessing import Pool
+from multiprocessing import Process
+from multiprocessing import Queue
+import subprocess
+def run_proc(name):
+    print(f'Run child process: {name} ({os.getpid()})')
+
+
+def long_run_task(name):
+    print(f'Run Task {name} ({os.getpid()})')
+    start = time.perf_counter()
+    time.sleep(random.random()*5)
+    end = time.perf_counter()
+    print(f'Task {name} runs {(end-start):.2f} seconds')
+
+
+def write(q):
+    print(f'Process to write: {os.getpid()}')
+    for value in ['A', 'B', 'C']:
+        print(f'Put {value} to queue...')
+        q.put(value)
+        time.sleep(random.random())
+
+
+def read(q):
+    print(f'Process to read: {os.getpid()}')
+    while True:
+        value = q.get(True)
+        print(f'Get {value} from queue.')
+
+
+if __name__ == '__main__':
+    # * Process Example
+    # print(f'Parent process: {os.getpid()}')
+    # p = Process(target=run_proc, args=('test',))
+    # print('Child process will start.')
+    # p.start()
+    # p.join()
+    # print('Child process end.')
+
+    # * Pool Example
+    '''
+    Pool的默认大小是CPU的核数。Like 16核的CPU就会default设置Pool的大小是16。Pool的大小就是同时执行进程的个数。
+    对Pool对象调用join()方法会等待所有子进程执行完毕，
+    调用join()之前必须先调用close()，调用close()之后就不能继续添加新的Process了。
+    '''
+    # print(f'Parent process: {os.getpid()}')
+    # p = Pool()
+    # for i in range(18):
+    #     p.apply_async(long_run_task, args=(i,))
+    # print('Waiting for all subprocesses done...')
+    # p.close()
+    # p.join()
+    # print('All subprocesses done!')
+
+    # * subprocess Example
+    '''
+    如果子进程还需要输入，则可以通过communicate()方法输入。
+    '''
+    # print('$ nslookup www.anqiwu.one')
+    # r = subprocess.call(['nslookup', 'www.anqiwu.one'])
+    # print('Exit code:', r)
+
+    # * 进程间通信
+    print(f'Parent Process: {os.getpid()}')
+    # 父进程创建Queue，并传给各个子进程：
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    # 启动子进程pw，写入:
+    pw.start()
+    # 启动子进程pr，读取:
+    pr.start()
+    # 等待pw结束:
+    pw.join()
+    # pr进程里是死循环，无法等待其结束，只能强行终止:
+    pr.terminate()
