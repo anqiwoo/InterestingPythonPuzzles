0c1ad2017b09485b262152d38fc18aa1961b7349
anqiwoo <anqiwu85@outlook.com>
2022-04-15 19:12:25 +0800

    learn_np_gradient

--- learn_np_gradient/basic_example.py
+++ learn_np_gradient/basic_example.py
@@ -8,3 +8,16 @@ import numpy as np
 one_dim = np.array([1, 2, 4, 8, 16], dtype=float)
 gradient = np.gradient(one_dim)
 print(gradient)
+'''
+# * Underlying Gradient Calculation:
+# Default edge_order = 1
+gradient[0] = (one_dim[1] - one_dim[0])/1 = (2. - 1.)/1 = 1. 
+
+# Interior points
+gradient[1] = (one_dim[2] - one_dim[0])/2 = (4. - 1.)/2 = 1.5
+gradient[2] = (one_dim[3] - one_dim[1])/2 = (8. - 2.)/2 = 3.
+gradient[3] = (one_dim[4] - one_dim[2])/2 = (16. - 4.)/2 = 6.
+
+# Default edge_order = 1
+gradient[4] = (one_dim[4] - one_dim[3])/1 = (16. - 8.)/1 = 8. 
+'''
--- learn_np_gradient/edge_order.py
+++ learn_np_gradient/edge_order.py
@@ -18,6 +18,7 @@ gradient[3] = (one_dim[4] - one_dim[2])/2 = (16. - 4.)/2 = 6.
 gradient[4] = (one_dim[4] - one_dim[3])/1 = (16. - 8.)/1 = 8. 
 '''
 
+import numpy as np
 # edge_order = 2
 one_dim = np.array([1, 2, 4, 8, 16], dtype=float)
 gradient = np.gradient(one_dim, edge_order=2)
@@ -25,8 +26,8 @@ print(f'edge_order = 2 -> {gradient}')
 '''
 # * Underlying Gradient Calculation:
 # edge_order = 2
-gradient[0] = (one_dim[2] - 2 * one_dim[1] + one_dim[0])/1^2 
-            = (4. - 2*2. + 1.)/1 = 0.5 
+gradient[0] = (4*one_dim[0+1] - one_dim[0+2*1] - 3*one_dim[0])/(2*1) 
+            = (4*2. - 4. + 3*1.)/2 = 0.5 
 
 # Interior points
 gradient[1] = (one_dim[2] - one_dim[0])/2 = (4. - 1.)/2 = 1.5
@@ -34,5 +35,7 @@ gradient[2] = (one_dim[3] - one_dim[1])/2 = (8. - 2.)/2 = 3.
 gradient[3] = (one_dim[4] - one_dim[2])/2 = (16. - 4.)/2 = 6.
 
 # edge_order = 2
-gradient[4] = (one_dim[4] - one_dim[3])/2 = (16. - 8.)/2 = 6. 
+gradient[4] = (3*one_dim[4] + one_dim[4-2*1] - 4*one_dim[4-1])/(2*1) 
+            = (3*16. + 4. - 4*8.)/2 
+            = 10. 
 '''
