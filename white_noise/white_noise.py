a390f26e3081ae46d0398343e179ac443dddb99b
anqiwu <anqiwu85@outlook.com>
2022-05-12 10:38:55 +0800

    22/5/12 Python FUn

--- white_noise/white_noise.py
+++ white_noise/white_noise.py
@@ -7,12 +7,10 @@ for num in nums:
     noise = np.random.normal(0, 1, num)
     corr = np.correlate(noise, noise, mode='full')
     plt.plot(corr)
-    # plt.savefig(f'figs/fig{num}.png')
     plt.savefig(f'fig{num}.png')
     plt.close()
 
 with imageio.get_writer('whiteNoise.gif', mode='I') as writer:
     for num in nums:
-        # image = imageio.imread(f'figs/fig{num}.png')
         image = imageio.imread(f'fig{num}.png')
         writer.append_data(image)
