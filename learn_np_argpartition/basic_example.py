0f3e2e13c59ec32f276e23b065a5a476c5368af3
anqiwu <anqiwu85@outlook.com>
2022-07-02 15:55:49 +0800

    update on 22/7/2

--- learn_np_argpartition/basic_example.py
+++ learn_np_argpartition/basic_example.py
@@ -5,9 +5,9 @@ numpy.argpartition(a, kth[, axis=- 1[, kind='introselect'[, order=None]]])
 
 # Basic Example
 import numpy as np
-one_dim = np.array([2, 3, 1, 5, 4])
+one_dim = np.array([3, 2, 1, 5, 4])
 # kth=0 -> partition based on 2(zero index).
 partitioned = np.argpartition(one_dim, 0)
-print(f'Unpartitioned array: {one_dim}')
+print(f'Unpartitioned array: {one_dimg}')
 print(f'Partitioned array index: {partitioned}')
 print(f'Partitioned array: {one_dim[partitioned]}')
